4aadd27411f4c4555a8beeb9bdb21502fa92ec37
leandrospan <leandrospan@outlook.com>
2019-10-16 12:47:05 -0300

    Desenvolvimento de Tela de Cadastro de Clientes - JFormattedTextField

--- CadCliente.java
+++ CadCliente.java
@@ -1,6 +1,10 @@
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.text.MaskFormatter;
+import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
 
 import java.awt.FlowLayout;
 import java.awt.Dimension;
@@ -8,6 +12,8 @@ import java.awt.GridLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.text.ParseException;
+import java.awt.Font;
 
 /**
  * Escreva a descrição da classe CadCliente aqui.
@@ -20,7 +26,7 @@ public class CadCliente extends JFrame
     /**
      * COnstrutor para objetos da classe CadCliente
      */
-    public CadCliente()
+    public CadCliente() throws ParseException
     {
         this.setTitle("-- QUIMERA | ERP -- CADASTRO DE CLIENTES ");
         this.setSize(600, 600);
@@ -28,15 +34,34 @@ public class CadCliente extends JFrame
         this.setLocationRelativeTo(null);
         
         JPanel jpnlHeader = new JPanel();
-        jpnlHeader.setLayout(new GridBagLayout());
+        jpnlHeader.setLayout(new FlowLayout());
+        /*jpnlHeader.setLayout(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(2, 2, 2, 2);
         jpnlHeader.setPreferredSize(new Dimension(600, 298));
-        JLabel jlblCodigoCli = new JLabel();
-        jlblCodigoCli.setText("Código: ");
+        */
+        JLabel jlblTitulo = new JLabel("CADASTRO DE CLIENTES");
+        jlblTitulo.setFont(new Font("Serif", Font.BOLD, 22));
+        JLabel jlblCodigoCli = new JLabel("Código: ");
+        JTextField jtfCodigoCli = new JTextField(5);
+        JLabel jlblNomeCli = new JLabel("Nome: ");
+        JTextField jtfNomeCli = new JTextField(15);
+        JLabel jlblSobrenomeCli = new JLabel("Sobrenome: ");
+        JTextField jtfSobrenomeCli = new JTextField(15);
+        JLabel jlblDataNascCli = new JLabel("Data de Nascimeto: ");
+        MaskFormatter mfDataNascCli = new MaskFormatter("##/##/####");
+        JFormattedTextField jftfDataNascCli = new JFormattedTextField(mfDataNascCli);
+        jftfDataNascCli.setColumns(7);
         
-        
+        jpnlHeader.add(jlblTitulo);
         jpnlHeader.add(jlblCodigoCli);
+        jpnlHeader.add(jtfCodigoCli);
+        jpnlHeader.add(jlblNomeCli);
+        jpnlHeader.add(jtfNomeCli);
+        jpnlHeader.add(jlblSobrenomeCli);
+        jpnlHeader.add(jtfSobrenomeCli);        
+        jpnlHeader.add(jlblDataNascCli);
+        jpnlHeader.add(jftfDataNascCli);
         
         this.add(jpnlHeader);
         this.setVisible(true);
